0aad8ecb94a51d70ff8cc870779359e87d83d4fd
elianenettoportal <elianenetto82@gmail.com>
2018-01-11 09:30:22 -0500

    commit untill 54 class step

---
+++ api/billingSummary/billingSummaryService.js
@@ -0,0 +1,10 @@
+const _= require('lodash')
+const BillingCycle = require('../billingCycle/billingCycle')
+
+//midalware for payment cicle
+function getSummary(req, res){
+  res.status(500).send('Ive got an error while implementing aggregation - spread item')
+  console.log('agregation..')
+  }
+
+module.exports = { getSummary}
