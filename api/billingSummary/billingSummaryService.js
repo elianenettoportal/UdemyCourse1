389ef4355ea8cf8a6eba19460162f21c89327586
elianenettoportal <elianenetto82@gmail.com>
2018-01-16 12:01:25 -0500

    final backend

--- api/billingSummary/billingSummaryService.js
+++ api/billingSummary/billingSummaryService.js
@@ -14,19 +14,7 @@ function getSummary(req, res){
           res.json(_.defaults(result[0], {credit: 0, debit: 0}))
         }
       }
-    )/**
-    BillingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debit: {$sum: "$debits.value"}}
-      }, function(error, result){
-            if(error){
-              res.status(500).json({errors: [error]})
-            }else{
-              res.json(_.defaults(result[0], {credit: 0, debit: 0}))
-            }
-          }
-        )**/
-/**res.status(500).send('Ive got an error while implementing aggregation - spread item')
-  console.log('agregation..')**/
+    )
 }
 
 module.exports = { getSummary}
